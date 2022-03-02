01ba40bf3fd870d66c84001ddc43a501fa464950
Goretti Alani <gorettialani@gmail.com>
2022-03-02 17:41:30 +1300

    add appbar and toolbar for header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from '../components/ui/header';
 
 function App() {
   return (
     <div className="App">
-      Hey!
+      <Header />
     </div>
   );
 }
